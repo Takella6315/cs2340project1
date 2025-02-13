2c03f78ccfbc84a9d0b8ab0798fcac6c9170c0d2
Teja Akella <takella6315@gmail.com>
2025-02-13 18:29:32 -0500

    order and purchase page done

--- movieStore/settings.py
+++ movieStore/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a!&p^m3$lb&r6q^22h_w_253xt%cql@fvn^3$22bh&(wv(&p^i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 
 # Application definition
